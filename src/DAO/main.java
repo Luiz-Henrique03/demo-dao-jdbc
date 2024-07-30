e8fe8367c50d0b6b39fb2c3567254d1026166b92
luizhenirque <luiz@kali>
2024-07-29 21:31:25 -0300

    Department created

---
+++ src/DAO/main.java
@@ -0,0 +1,12 @@
+package DAO;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department obj = new Department(1,"Teste");
+		
+		System.out.println(obj);
+	}
+
+}
