ab82b1cdf9a7f41963e9461d8d65132f7f7e3c00
luizhenirque <luiz@kali>
2024-08-17 14:48:20 -0300

    Seller created

--- src/DAO/main.java
+++ src/DAO/main.java
@@ -1,12 +1,15 @@
 package DAO;
 
+import java.util.Date;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Department obj = new Department(1,"Teste");
+		Seller sell = new Seller(21,"Bob","bob",new Date(),3000.0,obj);
 		
-		System.out.println(obj);
+		System.out.println(sell);
 	}
 
 }
